aabc0d8ff5fd03cf1fcbbbb7c09310c4e8c36724
dennismoha <dennismoha2@gmail.com>
2024-07-04 12:14:11 +0300

    removed unneccessary code in db.js

--- config/db.js
+++ config/db.js
@@ -13,22 +13,6 @@ const pool = mysql.createPool({
 });
 
 
-//docker
-// const pool = mysql.createPool({
-//   host: 'mysql',
-//   port: '3306',
-//   user: 'myuser',
-//   password: 'pass',
-//   database: 'mealplan'
-// });
 
-// const pool = mysql.createPool({
-//   host: 'localhost',
-//   user: 'root',
-//   port: '8012',
-//   database: 'meal_plan',
-//   password: 'root',
-//   multipleStatements: true // if abused may enhance grounds for injection as per the documentation
-// });
 
 module.exports = pool.promise();
