7e2bfdb454fce206ca8653950fe12df704642816
dennismoha <dennismoha2@gmail.com>
2024-07-09 22:04:38 +0300

    changed cors origin to all

--- setup_server.js
+++ setup_server.js
@@ -73,7 +73,7 @@ class MealPlanServer {
 
   #securityMiddleware(app) {
     const corsOptions = {
-      origin: "http://localhost:5173",
+      origin: "*",
       credentials: true, //access-control-allow-credentials:true
       optionSuccessStatus: 200,
     };
