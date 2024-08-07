7ec9b3eaec21eeadd97d55bb745b82ab2e642730
dennismoha <dennismoha2@gmail.com>
2024-08-07 12:57:30 +0300

    bullmq redis setup for prodc

--- globals/services/queues/base-queue.js
+++ globals/services/queues/base-queue.js
@@ -6,7 +6,6 @@ const { BullAdapter } = require("@bull-board/api/bullAdapter");
 const { ExpressAdapter } = require("@bull-board/express");
 const config = require("#mealplan/config.js");
 
-
 let serverAdapter = new ExpressAdapter();
 serverAdapter.setBasePath("/queues");
 
@@ -19,55 +18,51 @@ class BaseQueue {
     }
 
     try {
-      
-    if (process.env.NODE_ENV === "development") {
-      console.log('queue in development')
-      this.queue = new Queue(queuename, {
-        redis: { port: 6379, host: "localhost" },
+      if (process.env.NODE_ENV === "development") {
+        console.log("queue in development");
+        this.queue = new Queue(queuename, {
+          redis: { port: 6379, host: "localhost" },
+        });
+      } else if (process.env.NODE_ENV === "production") {
+        console.log("que in production");
+        this.queue = new Queue(queuename, {
+          redis: { tls:true, enableTLSForSentinelMode:false, url: config.REDIS_HOST, port: config.REDIS_PORT },
+        });
+        console.log("Running in production mode");
+      }
+
+      bullAdapters.push(new BullAdapter(this.queue));
+      bullAdapters = [...new Set(bullAdapters)];
+
+      serverAdapter.setBasePath("/queues");
+
+      createBullBoard({
+        queues: bullAdapters,
+        serverAdapter,
       });
-    } else if (process.env.NODE_ENV === "production") {
-      console.log('que in production')
-      this.queue = new Queue(queuename, {        
-        redis: {  url: config.REDIS_HOST,
-          port: config.REDIS_PORT,
-           },
+
+      // this.log = config.createLogger(`${queuename}Queue`);
+
+      this.queue.on("completed", (job) => {
+        job.remove();
       });
-      console.log("Running in production mode");
-    }
+      this.queue.on("global:completed", (jobId) => {
+        console.log(`Job ${jobId} completed`);
+      });
+      this.queue.on("global:stalled", (jobId) => {
+        console.log(`Job ${jobId} stalled`);
+      });
 
-    bullAdapters.push(new BullAdapter(this.queue));
-    bullAdapters = [...new Set(bullAdapters)];
+      if (this.addJob === undefined) {
+        throw new Error("getArea method must be implemented");
+      }
 
-    serverAdapter.setBasePath("/queues");
-
-    createBullBoard({
-      queues: bullAdapters,
-      serverAdapter,
-    });
-
-    // this.log = config.createLogger(`${queuename}Queue`);
-
-    this.queue.on("completed", (job) => {
-      job.remove();
-    });
-    this.queue.on("global:completed", (jobId) => {
-      console.log(`Job ${jobId} completed`);
-    });
-    this.queue.on("global:stalled", (jobId) => {
-      console.log(`Job ${jobId} stalled`);
-    });
-
-    if (this.addJob === undefined) {
-      throw new Error("getArea method must be implemented");
-    }
-
-    if (this.processJob === undefined) {
-      throw new Error("getArea method must be implemented");
-    }
+      if (this.processJob === undefined) {
+        throw new Error("getArea method must be implemented");
+      }
     } catch (error) {
-      console.error('base queue')
+      console.error("base queue");
     }
-
   }
 
   addJob(name, data) {
